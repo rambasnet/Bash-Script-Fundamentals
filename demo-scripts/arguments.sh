b72194b0b7f1a4d6e8730d86e2013be58a6d2793
Ram Basnet <rambasnet@gmail.com>
2019-02-19 22:32:49 -0700

    updated arguments.sh

--- demo-scripts/arguments.sh
+++ demo-scripts/arguments.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 echo "passing arguments to programs and functions"
+echo "Total Number of arguments: $#"
 echo "Program Name: $0"
 echo "First argument: $1"
 echo "Second argument: $2"
-echo "Quoted Values: $@"
-echo "Quoted Values: $*"
-echo "Total Number of arguments: $#"
+echo "Third argument: " $3
